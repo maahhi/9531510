851aad2224234a460b8ebe597450eb176bafb128
maahhi <mahi_jfg@rocketmail.com>
2016-11-10 18:17:49 +0330

    Update q1.java

--- AP-FirstHW-TA/src/q1.java
+++ AP-FirstHW-TA/src/q1.java
@@ -1,4 +1,27 @@
 
 public class q1 {
+	public static void mosalas(){
+		System.out.println("   /"+"\\"+"        /"+"\\");
+		System.out.println("  /  "+"\\"+"      /  "+"\\");
+		System.out.println(" /    "+"\\"+"    /    "+"\\");
+	}
+	public static void moraba(){
+		System.out.println("+------+  +------+");
+		System.out.println("|      |  |      |");
+		System.out.println("|      |  |      |");
+		System.out.println("+------+  +------+");
+
+	}
+	public static void us(){
+		System.out.println("|United|  |United|");
+		System.out.println("|States|  |States|");
+	}
+	public static void main(String args[]){
+		mosalas();
+		moraba();
+		us();
+		moraba();
+		mosalas();
+	}
 
 }
